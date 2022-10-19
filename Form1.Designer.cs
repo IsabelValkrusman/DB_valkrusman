f3b12d070ea3013c3a688f9f06edae19ed6e9026
opilane <opilane@HP-CZC6297B89>
2022-10-19 15:47:03 +0300

    YY

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,46 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Toode_txt = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.toode_txt = new System.Windows.Forms.TextBox();
+            this.hind_txt = new System.Windows.Forms.TextBox();
+            this.kogus_txt = new System.Windows.Forms.TextBox();
             this.Toode_lbl = new System.Windows.Forms.Label();
             this.Kategooria_lbl = new System.Windows.Forms.Label();
             this.Hind_lbl = new System.Windows.Forms.Label();
             this.Kogus_lbl = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
             this.Toode_gb = new System.Windows.Forms.PictureBox();
             this.Otsi_btn = new System.Windows.Forms.Button();
             this.Lisa_btn = new System.Windows.Forms.Button();
             this.Uuenda_btn = new System.Windows.Forms.Button();
             this.Kustuta_btn = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Kat_cbox = new System.Windows.Forms.ComboBox();
+            this.Lisa_kat_btn = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.Toode_gb)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // Toode_txt
+            // toode_txt
             // 
-            this.Toode_txt.Location = new System.Drawing.Point(206, 55);
-            this.Toode_txt.Name = "Toode_txt";
-            this.Toode_txt.Size = new System.Drawing.Size(130, 20);
-            this.Toode_txt.TabIndex = 0;
-            this.Toode_txt.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.toode_txt.Location = new System.Drawing.Point(206, 55);
+            this.toode_txt.Name = "toode_txt";
+            this.toode_txt.Size = new System.Drawing.Size(130, 20);
+            this.toode_txt.TabIndex = 0;
+            this.toode_txt.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // textBox2
+            // hind_txt
             // 
-            this.textBox2.Location = new System.Drawing.Point(206, 136);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(130, 20);
-            this.textBox2.TabIndex = 1;
+            this.hind_txt.Location = new System.Drawing.Point(206, 136);
+            this.hind_txt.Name = "hind_txt";
+            this.hind_txt.Size = new System.Drawing.Size(130, 20);
+            this.hind_txt.TabIndex = 1;
             // 
-            // textBox3
+            // kogus_txt
             // 
-            this.textBox3.Location = new System.Drawing.Point(206, 100);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(130, 20);
-            this.textBox3.TabIndex = 2;
-            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            this.kogus_txt.Location = new System.Drawing.Point(206, 100);
+            this.kogus_txt.Name = "kogus_txt";
+            this.kogus_txt.Size = new System.Drawing.Size(130, 20);
+            this.kogus_txt.TabIndex = 2;
+            this.kogus_txt.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // Toode_lbl
             // 
@@ -107,20 +110,15 @@
             this.Kogus_lbl.Text = "Kogus";
             this.Kogus_lbl.Click += new System.EventHandler(this.label4_Click);
             // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(206, 175);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(130, 20);
-            this.textBox4.TabIndex = 7;
-            // 
             // Toode_gb
             // 
-            this.Toode_gb.Location = new System.Drawing.Point(405, 55);
+            this.Toode_gb.Location = new System.Drawing.Point(414, 60);
             this.Toode_gb.Name = "Toode_gb";
-            this.Toode_gb.Size = new System.Drawing.Size(233, 140);
+            this.Toode_gb.Size = new System.Drawing.Size(224, 152);
+            this.Toode_gb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.Toode_gb.TabIndex = 8;
             this.Toode_gb.TabStop = false;
+            this.Toode_gb.Click += new System.EventHandler(this.Toode_gb_Click);
             // 
             // Otsi_btn
             // 
@@ -139,6 +137,7 @@
             this.Lisa_btn.TabIndex = 10;
             this.Lisa_btn.Text = "Lisa";
             this.Lisa_btn.UseVisualStyleBackColor = true;
+            this.Lisa_btn.Click += new System.EventHandler(this.Lisa_btn_Click);
             // 
             // Uuenda_btn
             // 
@@ -160,31 +159,66 @@
             // 
             // dataGridView1
             // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
             this.dataGridView1.Location = new System.Drawing.Point(56, 254);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(675, 179);
             this.dataGridView1.TabIndex = 13;
+            // 
+            // Kat_cbox
+            // 
+            this.Kat_cbox.FormattingEnabled = true;
+            this.Kat_cbox.Location = new System.Drawing.Point(206, 175);
+            this.Kat_cbox.Name = "Kat_cbox";
+            this.Kat_cbox.Size = new System.Drawing.Size(130, 21);
+            this.Kat_cbox.TabIndex = 14;
+            // 
+            // Lisa_kat_btn
+            // 
+            this.Lisa_kat_btn.Location = new System.Drawing.Point(307, 218);
+            this.Lisa_kat_btn.Name = "Lisa_kat_btn";
+            this.Lisa_kat_btn.Size = new System.Drawing.Size(92, 30);
+            this.Lisa_kat_btn.TabIndex = 15;
+            this.Lisa_kat_btn.Text = "Lisa kategooria";
+            this.Lisa_kat_btn.UseVisualStyleBackColor = true;
+            this.Lisa_kat_btn.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(797, 450);
+            this.Controls.Add(this.Lisa_kat_btn);
+            this.Controls.Add(this.Kat_cbox);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.Kustuta_btn);
             this.Controls.Add(this.Uuenda_btn);
             this.Controls.Add(this.Lisa_btn);
             this.Controls.Add(this.Otsi_btn);
             this.Controls.Add(this.Toode_gb);
-            this.Controls.Add(this.textBox4);
             this.Controls.Add(this.Kogus_lbl);
             this.Controls.Add(this.Hind_lbl);
             this.Controls.Add(this.Kategooria_lbl);
             this.Controls.Add(this.Toode_lbl);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.Toode_txt);
+            this.Controls.Add(this.kogus_txt);
+            this.Controls.Add(this.hind_txt);
+            this.Controls.Add(this.toode_txt);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
             this.Name = "Form1";
             this.Text = "Tooded";
@@ -197,20 +231,21 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox Toode_txt;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox toode_txt;
+        private System.Windows.Forms.TextBox hind_txt;
+        private System.Windows.Forms.TextBox kogus_txt;
         private System.Windows.Forms.Label Toode_lbl;
         private System.Windows.Forms.Label Kategooria_lbl;
         private System.Windows.Forms.Label Hind_lbl;
         private System.Windows.Forms.Label Kogus_lbl;
-        private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.PictureBox Toode_gb;
         private System.Windows.Forms.Button Otsi_btn;
         private System.Windows.Forms.Button Lisa_btn;
         private System.Windows.Forms.Button Uuenda_btn;
         private System.Windows.Forms.Button Kustuta_btn;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox Kat_cbox;
+        private System.Windows.Forms.Button Lisa_kat_btn;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,25 +14,107 @@ namespace DB_valkrusman
 {
     public partial class Form1 : Form
     {
-        SqlConnection connect = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename=|DataDirectory|\AppData\Tooded_DB.mdf;Integrated Security = True");
+        //SqlConnection connect = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename=|DataDirectory|\AppData\Tooded_DB.mdf;Integrated Security = True");
+        SqlConnection connect = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename=C:\Users\opilane\source\repos\DB_valkrusman\AppData\Tooded_DB.mdf;Integrated Security = True");
         SqlCommand cmd;
-        SqlDataAdapter adapter;
+        SqlDataAdapter adapter_toode, adapter_kat;
+       // C:\Users\opilane\source\repos\DB_valkrusman\AppData\Tooded_DB.mdf
+
         public Form1()
         {
             InitializeComponent();
             Naita_Andmed();
         }
+
+       
+
+
+        private void Lisa_btn_Click(object sender, EventArgs e)
+        {
+
+            if (toode_txt.Text.Trim() != string.Empty && kogus_txt.Text.Trim() != string.Empty && hind_txt.Text.Trim() != string.Empty && Kat_cbox.SelectedItem != null)
+            {
+                try
+                {
+                    cmd = new SqlCommand("INSERT INTO Toodedtable(Toodenimetus,Kogus,Hind,Pilt,Kategooria_id) VALUES (@toode,@kogus,@hind,@pilt,@kat)", connect);
+                    connect.Open();
+                    cmd.Parameters.AddWithValue("@toode", toode_txt.Text);
+                    cmd.Parameters.AddWithValue("@kogus", kogus_txt.Text);
+                    cmd.Parameters.AddWithValue("@hind", hind_txt.Text);//format andmebaasis ja vormis võrdsed
+                    cmd.Parameters.AddWithValue("@pilt", toode_txt.Text + ".jpg"); //format?
+                    cmd.Parameters.AddWithValue("@kat", Kat_cbox.SelectedIndex + 1);//id andmebaasist võtta
+                    cmd.ExecuteNonQuery();
+                    connect.Close();
+                    Kustuta_andmed();
+                    Naita_Andmed();
+
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Andmebaasiga viga!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Sisesta andmeid");
+            }
+
+        }
+
+
         public void Naita_Andmed()
         {
             connect.Open();
-            DataTable dt=new DataTable();
-            //cmd = new SqlCommand("SELECT * FROM Toodetable");
-            adapter = new SqlDataAdapter("SELECT * FROM Toodedtable",connect);
-            adapter.Fill(dt);
-            dataGridView1.DataSource = dt;
+            DataTable dt_toode=new DataTable();
+            adapter_toode = new SqlDataAdapter("SELECT * FROM Toodedtable",connect);
+            adapter_toode.Fill(dt_toode);
+            dataGridView1.DataSource = dt_toode;
+            
+            Toode_gb.Image = Image.FromFile("../../Images/info.jpg");
+
+            connect.Close();
+            Naita_Kat();
+        }
+
+        public void Kustuta_andmed()
+        {
+            toode_txt.Text = " ";
+            hind_txt.Text = " ";
+            kogus_txt.Text = " ";
+            Kat_cbox.Items.Clear();
+        }
+
+
+        private void Lisa_kat_btn_Click(object sender, EventArgs e)
+        {
+
+            cmd = new SqlCommand("INSERT INTO Kategooria(Kategooria_nimetus)VALUES(@kat)", connect);
+            connect.Open();
+            cmd.Parameters.AddWithValue("@kat", Kat_cbox.Text);
+            cmd.ExecuteNonQuery();
+            connect.Close();
+            Kustuta_andmed();
+            Naita_Kat();
+
+        }
+
+
+        public void Naita_Kat()
+        {
+            connect.Open();
+            adapter_kat = new SqlDataAdapter("SELECT Kategooria_nimetus FROM Kategooria", connect);
+            DataTable dt_kat = new DataTable();
+            adapter_kat.Fill(dt_kat);
+            foreach (DataRow nimetus in dt_kat.Rows)
+            {
+                Kat_cbox.Items.Add(nimetus["Kategooria_nimetus"]);
+            }
             connect.Close();
         }
 
+      
+
+      
 
 
 
@@ -63,6 +146,17 @@ namespace DB_valkrusman
         }
 
         private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        private void Toode_gb_Click(object sender, EventArgs e)
         {
 
         }
